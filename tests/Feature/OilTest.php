34cf9bef6658866cf27be13bfe26dbc6820afd49
Gary Chen <realgarychen@gmail.com>
2026-01-11 04:42:31 -0500

    Add more test cases

--- tests/Feature/OilTest.php
+++ tests/Feature/OilTest.php
@@ -79,3 +79,74 @@ test('current cant be smaller than previous', function () {
 
     $this->assertDatabaseCount('cars', 0);
 });
+
+test('current odometer is required', function () {
+    $this->travel(-1)->days();
+    $date = now();
+    $this->travel(1)->days();
+
+    $response = $this->postJson('/check/', [
+        'odometer_previous' => '2',
+        'date_previous' => $date
+    ]);
+
+    $this->assertDatabaseCount('cars', 0);
+});
+
+test('previous odometer is required', function () {
+    $this->travel(-1)->days();
+    $date = now();
+    $this->travel(1)->days();
+
+    $response = $this->postJson('/check/', [
+        'odometer_current' => '2',
+        'date_previous' => $date
+    ]);
+
+    $this->assertDatabaseCount('cars', 0);
+});
+
+test('date is required', function () {
+
+    $response = $this->postJson('/check/', [
+        'odometer_current' => '2',
+        'odometer_previous' => '1'
+    ]);
+
+    $this->assertDatabaseCount('cars', 0);
+});
+
+test('date must be valid', function () {
+
+    $response = $this->postJson('/check/', [
+        'odometer_current' => '2',
+        'odometer_previous' => '1',
+        'date' => 'h'
+    ]);
+
+    $this->assertDatabaseCount('cars', 0);
+});
+
+test('current odometer must be valid', function () {
+
+    $response = $this->postJson('/check/', [
+        'odometer_current' => 'h',
+        'odometer_previous' => '1',
+        'date' => '2000-01-01'
+    ]);
+
+    $this->assertDatabaseCount('cars', 0);
+});
+
+test('previous odometer must be valid', function () {
+
+    $response = $this->postJson('/check/', [
+        'odometer_current' => '2',
+        'odometer_previous' => 'h',
+        'date' => '2000-01-01'
+    ]);
+
+    $this->assertDatabaseCount('cars', 0);
+});
+
+// todo: test the oil change criteria?
