bc89350a34fe7ee4efba959d22d8ead243466955
Gary Chen <realgarychen@gmail.com>
2026-01-11 12:41:39 -0500

    Add comment for intended oil change condition

--- resources/views/components/show.blade.php
+++ resources/views/components/show.blade.php
@@ -5,6 +5,9 @@
     <p>Previous Oil Change Date: {{ $car->date_previous }}</p>
     <p>Submission Date: {{  $car->created_at }}</p>
 
+    <!-- Condition: more than 5000 km since last oil change
+        or more than 6 months since the last oil change
+        (assuming 6 months from submission date so results page doesn't change over time) -->
     @if (($car->odometer_current - $car->odometer_previous) > 5000 
     || $car->date_previous->diffInMonths($car->created_at) > 6)
         <p>The car is due for an oil change.</p>
