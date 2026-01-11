bde0595f11e1084d304b9d1faa342d288cccefb8
Gary Chen <realgarychen@gmail.com>
2026-01-11 01:54:38 -0500

    Create first Laravel route and view following tutorial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en" data-theme="lofi">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oil Change Challenge</title>
+    <link rel="preconnect" href="<https://fonts.bunny.net>">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<x-layout>
+    <x-slot:title>
+        Welcome
+    </x-slot:title>
+
+    <div class="max-w-2xl mx-auto">
+        <div class="card bg-base-100 shadow mt-8">
+            <div class="card-body">
+                <div>
+                    <h1 class="text-3xl font-bold">Welcome to the Oil Change Challenge!</h1>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
+</body>
+
+</html>
